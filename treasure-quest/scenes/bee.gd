13ddfc4522ae91af274068b5245804a97224168a
veronica-foltz <vfoltz1520@gmail.com>
2026-06-16 12:31:46 -1000

    edited obstacles
    edited obstacles

--- treasure-quest/scenes/bee.gd
+++ treasure-quest/scenes/bee.gd
@@ -2,5 +2,17 @@ extends Area2D
 
 func _on_body_entered(body):
 
-	if body.name == "Player":
-		body.position = Vector2(100, 300)
+	print("TOUCHED:", name)
+	body.position = Vector2(100, 300)
+
+
+func _on_bee_3_body_entered(body: Node2D):
+	print("TOUCHED:", name)
+
+	body.position = Vector2(100, 300)
+
+
+func _on_bee_2_body_entered(body: Node2D):
+	print("TOUCHED:", name)
+
+	body.position = Vector2(100, 300)
