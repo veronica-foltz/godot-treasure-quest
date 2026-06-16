0ff7b8e3db06b1feb4e505ef58e614f9d785eab3
veronica-foltz <vfoltz1520@gmail.com>
2026-06-16 11:57:03 -1000

    fixed bees
    fixed bees

--- treasure-quest/scenes/bee.gd
+++ treasure-quest/scenes/bee.gd
@@ -1,11 +1,6 @@
 extends Area2D
 
+func _on_body_entered(body):
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if body.name == "Player":
+		body.position = Vector2(100, 300)
