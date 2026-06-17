72b00f653a7db9d0c44f3ba5e64b9fce51c4a648
veronica-foltz <vfoltz1520@gmail.com>
2026-06-17 11:27:08 -1000

    made alien move
    made alien move

--- treasure-quest/scenes/player.gd
+++ treasure-quest/scenes/player.gd
@@ -1,23 +1,35 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
+const SPEED = 200
 
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -400
+
+const GRAVITY = 900
+
+@onready var anim = $AnimatedSprite2D
 
 func _physics_process(delta):
 
+	var direction = Input.get_axis("left", "right")
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y += GRAVITY * delta
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction != 0:
 		velocity.x = direction * SPEED
+		anim.flip_h = direction < 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = 0
 	move_and_slide()
-
-
+	if not is_on_floor():
+		anim.play("jump")
+	elif Input.is_action_pressed("crouch"):
+		anim.play("crouch")
+	elif direction != 0:
+		anim.play("walk")
+	else:
+		anim.play("idle")
+		
 func _on_bee_body_entered(body):
 	if body.name == "Player":
 		body.position = Vector2(100, 300)
