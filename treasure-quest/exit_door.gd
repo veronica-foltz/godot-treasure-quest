4ae3e8dd6e59d8494e9b1981cdee663c5494e007
veronica-foltz <vfoltz1520@gmail.com>
2026-06-21 16:27:33 -1000

    connected the exit door to levels
    connected exit door to various levels

--- treasure-quest/exit_door.gd
+++ treasure-quest/exit_door.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var next_level = "res://scenes/level_3.tscn"
+@export var next_level: String = ""
 
 func _ready():
 
@@ -9,7 +9,6 @@ func _ready():
 func _on_body_entered(body):
 
 	if body.name == "Player":
-		print("DOOR TOUCHED:", body.name)
 		$AnimatedSprite2D.frame = 1
 		await get_tree().create_timer(0.5).timeout
 		get_tree().change_scene_to_file(next_level)
