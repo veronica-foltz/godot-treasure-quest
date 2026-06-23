c71951128a9153fc6d42331548b5808ab6efdbcf
veronica-foltz <vfoltz1520@gmail.com>
2026-06-23 09:57:19 -1000

    added health bar
    added health bar

--- treasure-quest/scenes/player.gd
+++ treasure-quest/scenes/player.gd
@@ -5,6 +5,7 @@ const JUMP_VELOCITY = -400
 const GRAVITY = 900
 
 @onready var anim = $AnimatedSprite2D
+@onready var health_bar = get_tree().current_scene.get_node("CanvasLayer/HealthBar")
 
 var health = 3
 var start_position = Vector2(100, 300)
@@ -19,17 +20,15 @@ func _physics_process(delta):
 		velocity.y += GRAVITY * delta
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if is_hurt:
+		move_and_slide()
+		return
 	if direction != 0:
 		velocity.x = direction * SPEED
 		anim.flip_h = direction < 0
 	else:
 		velocity.x = 0
 	move_and_slide()
-	
-	if is_hurt:
-		move_and_slide()
-		return
-	
 	if not is_on_floor():
 		anim.play("jump")
 	elif Input.is_action_pressed("crouch"):
@@ -53,6 +52,7 @@ func take_damage(enemy_position):
 	if can_take_damage == false:
 		return
 	health -= 1
+	health_bar.value = health
 	print("Health:", health)
 	can_take_damage = false
 	is_hurt = true
@@ -71,7 +71,7 @@ func take_damage(enemy_position):
 	if health <= 0:
 		position = Vector2(100, 300)
 		health = 3
-		#health_bar.value = health
+		health_bar.value = health
 	await get_tree().create_timer(0.5).timeout
 	is_hurt = false
 	await get_tree().create_timer(0.5).timeout
